bb184c87cd7939c9ba567b214e7e60faaf4522ac
munjungmin <answjdals77@gmail.com>
2023-01-31 10:44:10 +0900

    Header(머리글)와 Tooltip 작성

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    [Header("General Setup Settings")]
+    [Tooltip("How fast ship moves up and down based upon plyaer input")] 
     [SerializeField] float controlSpeed = 10f;
-    [SerializeField] float xRange = 5f;
-    [SerializeField] float yRange = 5f;
+    
+    [Tooltip("How fast player moves horizontally")] [SerializeField] float xRange = 5f;
+    [Tooltip("How fast player moves vertically")] [SerializeField] float yRange = 5f;
+
+    [Header("Laser gun array")]
+    [Tooltip("Add all player lasers here")]
     [SerializeField] GameObject[] lasers;
 
+    [Header("Screen position based tuning")]
     [SerializeField] float positionPitchFactor = -10f; 
+    [SerializeField] float positionYawFactor = 2f;
+
+    [Header("Player input based tuning")] 
     [SerializeField] float controlPitchFactor = -10f;
-    [SerializeField] float positionYawFactor = 2f; 
     [SerializeField] float controlRollFactor = -10f;
 
     float xThrow, yThrow;
