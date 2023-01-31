4b31bcd055537fbfb8e1b8d1b63df1bf22a1ba94
munjungmin <answjdals77@gmail.com>
2023-01-31 14:34:03 +0900

    장애물에 부딪히면 씬 다시 로드

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other)
-    {
-        Debug.Log(this.name + "--Collided with--" + other.gameObject.name);  
-    }
+    [SerializeField] float loadDelay = 1f;
 
     void OnTriggerEnter(Collider other) {
-        Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");  // string interpolation
+        
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
