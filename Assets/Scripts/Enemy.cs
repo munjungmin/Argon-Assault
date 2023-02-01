0d1a8704b05c40f303e8291e593818d3be60c619
munjungmin <answjdals77@gmail.com>
2023-02-01 11:08:44 +0900

    런타임에서 객체 생성하기
    생성, 자식객체로 정리, particle에SelfDestruct script 추가

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    void OnParticleCollision(GameObject other) {
-        //Debug.Log($"{this.name} I'm hit! by {other.gameObject.name}");
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    void OnParticleCollision(GameObject other) 
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;  // 부모 게임오브젝트로 들어가게 함?
         Destroy(gameObject);
     }
 }
